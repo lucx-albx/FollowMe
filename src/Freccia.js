769fc4da40dceb5ca03d8006735285eebe57e241
Luca <117486221+lucx-albx@users.noreply.github.com>
2023-10-02 08:30:45 +0200

    .

--- src/Freccia.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Freccia.css'
-import FrecciaDir from './Image/Freccia.png'
-
-const Freccia = ({ className }) => {
-    return (<img src={FrecciaDir} className={className}/>)
-}
-
-export default Freccia
