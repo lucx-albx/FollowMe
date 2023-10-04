09d828c9f1c7b58baec7794e152e2c12d2689faa
Luca <117486221+lucx-albx@users.noreply.github.com>
2023-10-04 19:31:22 +0200

    .

---
+++ src/Footer.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Footer.css';
+import { NavLink, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationArrow, faHouse, faQrcode } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
+    const EmIcon = <FontAwesomeIcon icon={faHouse} className='pointer'/>
+    const QrIcon = <FontAwesomeIcon icon={faQrcode} className='pointer'/>
+    const SiIcon = <FontAwesomeIcon icon={faLocationArrow} className='pointer'/>
+
+    const [icone, setIcone] = useState({
+        em: [EmIcon, "ss"],
+        qr: [QrIcon, "ss"],
+        si: [SiIcon, "ss"]
+    })
+
+    // Posizione corrente ( hook useLocation() )
+    const location = useLocation();
+
+    const footerRef = useRef(null);
+
+    const sezione = (cls) => {
+        //serve per togliere la freccia dalla navbar
+        if (cls !== "si"){
+            let frecciaTornaIndietro = document.querySelector(".posizione-freccia").classList
+            frecciaTornaIndietro.add("invisibile")
+        }
+
+        const nuoveIcone = {
+            em: cls === 'em' ? [EmIcon, "ns"] : [EmIcon, "ss"],
+            qr: cls === 'qr' ? [QrIcon, "ns"] : [QrIcon, "ss"],
+            si: cls === 'si' ? [SiIcon, "ns"] : [SiIcon, "ss"],
+        };
+
+        setIcone(nuoveIcone);
+    };
+
+    useEffect(() => {
+        // Controllo dela posizione corrente per impostare l'icona
+        if (location.pathname === '/QrCode') {
+            sezione('qr');
+        } else if (location.pathname === '/') {
+            sezione('em');
+        } else if (location.pathname === '/Sicurezza' || location.pathname === '/Sicurezza/Percorso') {
+            sezione('si');
+        } else {
+            sezione('rest');
+        }
+    }, [location.pathname]);
+
+    return (
+        <div className='MainFooter' ref={footerRef}>
+            <div className='FooterIcon'>
+
+                <NavLink to={"/QrCode"} onClick={()=>sezione('qr')} style={{ textDecoration: 'none', color: '#319547' }} className={icone.qr[1] === "ns" ? 'SezioneImpostata' : 'iconaStyle'}>
+                    {icone.qr[0]}
+                </NavLink>
+
+                <NavLink to={"/"} onClick={()=>sezione('em')} style={{ textDecoration: 'none', color: '#319547' }} className={icone.em[1] === "ns" ? 'SezioneImpostataH' : 'iconaStyleH'}>
+                    {icone.em[0]}
+                </NavLink>
+
+                <NavLink to={"/Sicurezza"} onClick={()=>sezione('si')} style={{ textDecoration: 'none', color: '#319547' }} className={icone.si[1] === "ns" ? 'SezioneImpostata' : 'iconaStyle'}>
+                    {icone.si[0]}
+                </NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/QrCode.js
@@ -0,0 +1,79 @@
+/*
+- Errore installazione libreria react-qr-reader:
+
+  npm i react-qr-reader
+  npm ERR! code ERESOLVE
+  npm ERR! ERESOLVE unable to resolve dependency tree
+  npm ERR!
+  npm ERR! While resolving: followme@0.1.0
+  npm ERR! Found: react@18.2.0
+  npm ERR! node_modules/react
+  npm ERR!   react@"^18.2.0" from the root project
+  npm ERR!
+  npm ERR! Could not resolve dependency:
+  npm ERR! peer react@"^16.8.0 || ^17.0.0" from react-qr-reader@3.0.0-beta-1
+  npm ERR! node_modules/react-qr-reader
+  npm ERR!   react-qr-reader@"*" from the root project
+  npm ERR!
+  npm ERR! Fix the upstream dependency conflict, or retry
+  npm ERR! this command with --force or --legacy-peer-deps
+  npm ERR! to accept an incorrect (and potentially broken) dependency resolution.
+
+
+- Il pacchetto react-qr-reader richiede una versione di react compresa tra "^16.8.0" e "^17.0.0", ma nel progetto è presente react versione "18.2.0", che non è compatibile con queste richieste.
+
+- Per risolvere --> installazione forzata con il flag --force:
+  npm install react-qr-reader --force
+
+- Quando si installano i moduli da zero bisogna usare --force (esempio: npm install --force)
+*/
+
+import './QrCode.css'
+import React from 'react';
+import { QrReader } from 'react-qr-reader';
+import { isMobile } from 'react-device-detect';
+import qrInfo from './Image/QrInfo.png'
+
+const QrCode = () => {
+	window.scrollTo(0,0)
+
+	const openLink = (url) => {
+		window.location.href = url
+		console.log(url)
+	}
+
+	return (
+		<div className='row p-2'>
+			{ isMobile ? 
+				<div className="col-12 MainQrCode mt-6">
+					<QrReader
+						constraints = {{
+							facingMode: { exact: "environment" }
+						}}
+						onResult={(result) => {
+							if (!!result) {
+								openLink(result?.text)
+							}
+						}}
+					/> 
+				</div>
+
+				:
+
+				(	
+					<div className='col-12 styleNoQr mt-6'>
+						<img src={qrInfo} class="img-fluid imgNoQr"/>
+						<bold className='text-center black-title-qr' style={{marginTop:'30px'}}>Usa lo scanner da telefono</bold>
+						<p className='p-uscita'>
+							Ci dispiace, ma per motivi logistici abbiamo deciso di non far utilizzare 
+							lo scanner di QR Code sul computer. Recati sul tuo dispositivo 
+							mobile per utilizzare lo scanner.
+						</p>
+					</div>
+				)
+			}
+		</div>
+		)
+	}
+
+export default QrCode
