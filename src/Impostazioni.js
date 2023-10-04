79e3b655e0d9db26385d3593eccb6421c9b7ea86
Luca <117486221+lucx-albx@users.noreply.github.com>
2023-10-04 17:04:38 +0200

    ultimate

---
+++ src/Impostazioni.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Impostazioni.css'
+
+
+const Impostazioni = () => {
+    return (
+        <div className='mt-6'>
+            Impostazioni
+        </div>
+    )
+}
+
+export default Impostazioni
