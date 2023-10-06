13e281184142274d39f1f261a98bbccbc217f0ac
Luca <117486221+lucx-albx@users.noreply.github.com>
2023-10-06 16:15:29 +0200

    Aggiunti percorsi uscite ed ingressi

---
+++ src/Percorso.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect, useContext } from 'react';
+import './Percorso.css';
+import Freccia from './Freccia.js';
+import { variabili } from './App.js'
+import OwnBtn from './OwnBtn';
+import { faArrowLeft, faArrowRight, faRefresh } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+const Percorso = () => {
+    let impostaVar = useContext(variabili)
+    const partenza = impostaVar[0].newVar.partenza.toLowerCase().replace(/\s+/g, '')
+    const arrivo = impostaVar[0].newVar.arrivo.toLowerCase().replace(/\s+/g, '')
+    const tuttiPercorsi = impostaVar[0].newVar.opzioni
+    const [directionIndex, setDirectionIndex] = useState(0)
+    const [maxReached, setMaxReached] = useState(false)
+    const urlUscite = process.env.REACT_APP_URL_USCITE
+    const urlIngressi = process.env.REACT_APP_URL_ENTRATE
+
+    useEffect(() => {
+        caricaPercorso()
+    }, [])
+    
+    const caricaPercorso =()=>{
+		let datPart = partenza
+		let datArr = arrivo
+		let url = ""
+
+        if(datPart === "ingresso"){
+            url = urlIngressi
+        } else if(datArr === "uscita"){
+            url = urlUscite
+        }
+
+        fetch(url)
+        .then((testo) => testo.json())
+        .then((data)=>{
+            let part = datPart
+            let arr = datArr
+            let percorsiEsistenti = false
+
+            data.map((elem, i)=>{
+                if(part ===  elem.partenza.toLowerCase().replace(/\s+/g, '') &&  
+                    arr ===  elem.destinazione.toLowerCase().replace(/\s+/g, ''))
+                    percorsiEsistenti = true
+                else if(percorsiEsistenti !== true)
+                    percorsiEsistenti = false
+            })
+            
+            if (percorsiEsistenti === true){
+                impostaVar[0].setNewVar({
+                    "partenza": datPart,
+                    "arrivo": datArr,
+                    "opzioni": data
+                })
+            }
+            
+        })
+	}
+
+    const getDirectionClass = () => {
+        if (tuttiPercorsi) {
+            const percorsoCorrispondente = tuttiPercorsi.find((elem) =>
+                partenza === elem.partenza.toLowerCase().replace(/\s+/g, '') &&
+                arrivo === elem.destinazione.toLowerCase().replace(/\s+/g, '')
+            )
+
+            if (percorsoCorrispondente) {
+                const directions = percorsoCorrispondente.passaggi.map((elem) =>
+                    elem.direzione.toLowerCase()
+                )
+
+                const descrizione = percorsoCorrispondente.passaggi.map((elem) =>
+                    elem.descrizione
+                )
+
+                if (directionIndex < directions.length) {
+                    return [directions[directionIndex], descrizione[directionIndex]]
+                } else {
+                    setMaxReached(true)
+                }
+            }
+        }
+        
+        return ["", ""]
+    }
+
+    const ricomincia = () => {
+        setDirectionIndex(0)
+        setMaxReached(false)
+    }
+
+    const ultimaIndicazione = () => {
+        if (directionIndex !== 0) {
+            setDirectionIndex(directionIndex - 1)
+        }
+    }
+
+    const prossimaIndicazione = () => {
+        setDirectionIndex(directionIndex + 1)
+    }
+
+    const renderFreccia = () => {
+        const directionClass = getDirectionClass()[0] + " RidFrec"
+        const descDirection = getDirectionClass()[1]
+        return (
+            <>
+                <Freccia className={directionClass} />
+                <strong className='text-center'>{descDirection}</strong>
+                <div className='menuBtn'>
+                    <OwnBtn icona={faArrowRight} onClick={ultimaIndicazione} />
+                    <OwnBtn icona={faRefresh} onClick={ricomincia} />
+                    <OwnBtn icona={faArrowLeft} onClick={prossimaIndicazione} />
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <div className='MainPercorso'>
+            {!maxReached ? (
+                renderFreccia()
+            ) : (
+                <>
+                    <FontAwesomeIcon icon={faLocationDot} style={{ fontSize: '60px', color: '#319547' }} />
+                    <p style={{ fontSize: '20px' }}><strong>Sei arrivato a destinazione!</strong></p>
+                    {/* <div onClick={ricomincia} className='btn-repeat pulse-green'>RIPETI</div> */}
+                </>
+            )}
+        </div>
+    )
+}
+
+export default Percorso
