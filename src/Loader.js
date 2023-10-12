9ff1acf5e87f225aa8259ae2b415bf04c95b6e44
MartiniGabriele <120101852+MartiniGabriele@users.noreply.github.com>
2023-10-12 12:55:55 +0200

    commit scr con tutti i file (spero)

---
+++ src/Loader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Loader.css'
+
+const Loader = () => {
+    return (
+        <div className="dot-spinner invisibile">
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+            <div className="dot-spinner__dot"></div>
+        </div>
+    )
+}
+
+export default Loader
